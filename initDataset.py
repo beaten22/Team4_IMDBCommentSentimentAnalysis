322b418f0f3ca3aadde3ca54f1b3d64c0ec661ec
Burkay Maras <burkaymaras@hotmail.com>
2024-04-30 15:03:37 +0300

    Dropped missing and duplicated columns from dataset

--- initDataset.py
+++ initDataset.py
@@ -1,5 +1,11 @@
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt
-dataset_directory="Dataset\IMDB Dataset.csv"
+dataset_directory="Dataset/IMDB_Dataset.csv"
 df_dataset=pd.read_csv(dataset_directory)
+missing_values = df_dataset[df_dataset.isnull().any(axis=1)] # there wasn't missing column
+duplicates = df_dataset[df_dataset.duplicated(subset=["review", "sentiment"], keep=False)]
+
+#I dropped duplicated rows from dataset
+cleaned_df = df_dataset.drop_duplicates(subset=["review", "sentiment"])
+cleaned_df = cleaned_df.dropna()
